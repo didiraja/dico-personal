28886bf5b380b2fe7af0b7df50cd80fbd69fc75d
Dico <didiraja@hotmail.com>
2022-08-17 17:12:27 -0300

    task style complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,74 @@
 import type { NextPage } from 'next';
-import { Button } from '@blueprintjs/core';
+import { useState, useEffect } from 'react';
+
+import { Button, Callout } from '@blueprintjs/core';
 
 const Home: NextPage = () => {
-  const crossCount = 2;
-  const dinnerCount = 0;
+  const [crossCount, setCross] = useState(2);
+  const [crossStatus, setStatus] = useState({
+    intent: '',
+    icon: '',
+    title: '',
+    description: '',
+  });
 
-  const statusCross = 'OK';
-  const statusDesc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis sapien magna..';
+  function updateCross() {
+    setCross(crossCount + 1);
+
+    // to do:: more than 5
+  }
+
+  useEffect(() => {
+    const STATUS = {
+      good: {
+        intent: 'success',
+        icon: 'heart',
+        title: 'Ótimo',
+        description: 'Boa! Em busca do shape.',
+      },
+      ok: {
+        intent: 'primary',
+        icon: 'tick-circle',
+        title: 'OK',
+        description: 'Fez o mínimo. Agora é melhorar essa meta.',
+      },
+      bad: {
+        intent: 'danger',
+        icon: 'cross',
+        title: 'Ruim',
+        description: 'Lembra da qualidade de vida.',
+      },
+    };
+
+    if (crossCount > 3) return setStatus(STATUS.good);
+
+    if (crossCount === 3) return setStatus(STATUS.ok);
+
+    if (crossCount < 3) return setStatus(STATUS.bad);
+  }, [crossCount]);
 
   return (
-    <div className="p-5">
+    <div className="w-full">
       <div className="week-title">Semana de *segunda* a *sexta*</div>
 
       <div className="main-wrapper">
         <div className="task-wrapper">
           <p className="cross-label">Cross na semana:</p>
+
           <p className="cross-count">{crossCount}</p>
-          <Button large intent="success" icon="insert" />
+
+          <div className="box-button">
+            <Button large icon="plus" intent="primary" onClick={updateCross} />
+          </div>
+
           <div className="status-box">
-            <p className="status">Status: {statusCross}</p>
+            <Callout icon={crossStatus.icon} intent={crossStatus.intent} title={`Status: ${crossStatus.title}`}>
+              {crossStatus.description}
+            </Callout>
+
+            {/* <p className="status">Status: {crossStatus.title}</p>
             <hr />
-            <div className="description">{statusDesc}</div>
+            <div className="description">{crossStatus.description}</div> */}
           </div>
         </div>
 
